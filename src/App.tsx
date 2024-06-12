fd6731ee878acb28f208bbe95348c9feadb6fced
Bassaamm <bassamalqrni@gmail.com>
2024-06-12 21:43:00 +0300

    the cochues are ready, but the texture need a lot of work

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,83 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Canvas, useLoader } from "@react-three/fiber";
+import { OrbitControls, useGLTF } from "@react-three/drei";
+import * as THREE from "three";
 
-function App() {
-  const [count, setCount] = useState(0)
+function Model() {
+  const gltf = useGLTF("/Couch.glb");
+  const aoMap = useLoader(
+    THREE.TextureLoader,
+    "/textures/Fabric035_2K_AmbientOcclusion.jpg"
+  );
+  const colorMap = useLoader(
+    THREE.TextureLoader,
+    "/textures/Fabric035_2K_Color.jpg"
+  );
+  const displacementMap = useLoader(
+    THREE.TextureLoader,
+    "/textures/Fabric035_2K_Displacement.jpg"
+  );
+  const normalMap = useLoader(
+    THREE.TextureLoader,
+    "/textures/Fabric035_2K_Normal.jpg"
+  );
+  const roughnessMap = useLoader(
+    THREE.TextureLoader,
+    "/textures/Fabric035_2K_Roughness.jpg"
+  );
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  gltf.scene.traverse((o) => {
+    console.log(o);
+    if (o.name === "Cube010_1")
+      // @ts-ignore
+      o.material.map = useLoader(
+        THREE.TextureLoader,
+        "/textures/Fabric_013_Base_Color.jpg"
+      );
+    if (o.name === "Cube010_2")
+      // @ts-ignore
+      o.material.map = useLoader(
+        THREE.TextureLoader,
+        "/textures/Fabric040_2K_Color.jpg"
+      );
+    if (o.name === "Cube010_3")
+      // @ts-ignore
+      o.material.map = useLoader(
+        THREE.TextureLoader,
+        "/textures/Fabric038_2K_Color.jpg"
+      );
+    if (o.name === "Cube010_4")
+      // @ts-ignore
+      o.material.map = useLoader(
+        THREE.TextureLoader,
+        "/textures/Fabric035_2K_Color.jpg"
+      );
+    // @ts-ignore
+    if (o.isMesh) {
+      // @ts-ignore
+      o.material.aoMap = aoMap;
+      // @ts-ignore
+      // o.material.map = colorMap;
+      // @ts-ignore
+      // o.material.displacementMap = displacementMap;
+      // @ts-ignore
+      o.material.normalMap = normalMap;
+      // @ts-ignore
+      o.material.roughnessMap = roughnessMap;
+    }
+  });
+
+  return <primitive object={gltf.scene} />;
 }
 
-export default App
+function App() {
+  return (
+    <Canvas>
+      <ambientLight intensity={0.4} />
+      <directionalLight intensity={1} position={[0, 0, 5]} />
+      <Model />
+      <OrbitControls />
+    </Canvas>
+  );
+}
+
+export default App;
